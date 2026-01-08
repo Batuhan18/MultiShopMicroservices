8ec653e202dcc7e81b95792d47db8906ed87c63e
Ahmet Batuhan Avcıoğlu <54512066+Batuhan18@users.noreply.github.com>
2026-01-08 15:43:18 +0300

    ...

---
+++ Frontends/MultiShopMicroservices.Mikorservis.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetaiImagelSliderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShopMicroservices.Mikorservis.WebUI.ViewComponents.ProductDetailViewComponents
+{
+    public class _ProductDetaiImagelSliderComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
