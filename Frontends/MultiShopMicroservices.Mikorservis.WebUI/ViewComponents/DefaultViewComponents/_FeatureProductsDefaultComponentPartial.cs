cadc1933536191c0bb6cf47944aa346dbba623da
Ahmet Batuhan Avcıoğlu <54512066+Batuhan18@users.noreply.github.com>
2026-01-08 11:52:21 +0300

    ...

---
+++ Frontends/MultiShopMicroservices.Mikorservis.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShopMicroservices.Mikorservis.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _FeatureProductsDefaultComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
