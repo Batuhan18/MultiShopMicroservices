65768c99eb35935c584dd97d287d7a88585f44ef
Ahmet Batuhan Avcıoğlu <54512066+Batuhan18@users.noreply.github.com>
2026-02-14 19:30:09 +0300

    ...

--- Frontends/MultiShopMicroservices.Mikorservis.WebUI/Controllers/ProductListController.cs
+++ Frontends/MultiShopMicroservices.Mikorservis.WebUI/Controllers/ProductListController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShopMicroservices.DtoLayer.CommentDtos;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShopMicroservices.Mikorservis.WebUI.Controllers
 {
     public class ProductListController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductListController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index(string id)
         {
             ViewBag.i = id;
@@ -13,6 +23,31 @@ namespace MultiShopMicroservices.Mikorservis.WebUI.Controllers
         public IActionResult ProductDetail(string id)
         {
             ViewBag.x = id;
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult AddComment(string id)
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddComment(CreateCommentDto createCommentDto)
+        {
+            createCommentDto.ImageUrl = "test";
+            createCommentDto.Rating = 1;
+            createCommentDto.CreatedDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            createCommentDto.Status = false;
+            createCommentDto.ProductId = "694d19ab6f5b5637c5583a35";
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCommentDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7214/api/Comments", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default", new { area = "Admin" });
+            }
             return View();
         }
     }
