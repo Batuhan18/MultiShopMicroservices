974db3ac086a6bcd18e90e28c7a7a10465c9073a
Ahmet Batuhan Avcıoğlu <54512066+Batuhan18@users.noreply.github.com>
2026-03-06 14:32:01 +0300

    ...

---
+++ Frontends/MultiShopMicroservices.Mikorservis.WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShopMicroservices.Mikorservis.WebUI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
