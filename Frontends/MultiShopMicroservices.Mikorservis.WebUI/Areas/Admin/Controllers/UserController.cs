95e01410041aa89b6daa460225854ad9a4994df5
Ahmet Batuhan Avcıoğlu <54512066+Batuhan18@users.noreply.github.com>
2026-03-30 15:05:25 +0300

    ...

---
+++ Frontends/MultiShopMicroservices.Mikorservis.WebUI/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShopMicroservices.Mikorservis.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        public async Task<IActionResult> UserList()
+        {
+            return View();
+        }
+    }
+}
