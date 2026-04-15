97239f6eb8c9d0a819abbd8cb1eeadb8feacf9ec
Ahmet Batuhan Avcıoğlu <54512066+Batuhan18@users.noreply.github.com>
2026-04-15 22:46:53 +0300

    ...

---
+++ Frontends/MultiShopMicroservices.Mikorservis.WebUI/Controllers/MailController.cs
@@ -0,0 +1,41 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MultiShopMicroservices.Mikorservis.WebUI.Models;
+
+namespace MultiShopMicroservices.Mikorservis.WebUI.Controllers
+{
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult SendMail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMail(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddress = new MailboxAddress("MultiShop Katalog", "batuhanavciogl.18@gmail.com");
+            mimeMessage.From.Add(mailboxAddress);
+            
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = mailRequest.Content;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = mailRequest.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("batuhanavciogl.18@gmail.com", "lifphosrawztamgz");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            return View();
+        }
+    }
+}
---
+++ Frontends/MultiShopMicroservices.Mikorservis.WebUI/Models/MailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShopMicroservices.Mikorservis.WebUI.Models
+{
+    public class MailRequest
+    {
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+    }
+}
